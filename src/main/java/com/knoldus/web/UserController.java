33227c14d0c2ba2f3e507489ee4b0f4bfca14015
Pratibha-Knoldus <pratibha.yadav@knoldus.com>
2022-12-14 18:59:55 +0530

    updated

--- src/main/java/com/knoldus/web/UserController.java
+++ src/main/java/com/knoldus/web/UserController.java
@@ -4,10 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author itguang
- * @create 2018-01-05 8:23
- **/
 @RestController
 public class UserController {
 
